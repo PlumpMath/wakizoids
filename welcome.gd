c37533b521f1bccf4cd1bf3eec3add906050176a
Pete Goodwin <github@imekon.org>
2017-11-05 11:23:24 +0000

    added welcome screen

---
+++ welcome.gd
@@ -0,0 +1,14 @@
+extends PanelContainer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+func _onStart():
+	get_tree().change_scene("res://main.tscn")
