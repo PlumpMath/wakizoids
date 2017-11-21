e029695725e92c1a278bee6ae546c122d4fcab3d
Pete Goodwin <pgoodwin@imekon.org>
2017-11-21 11:05:03 +0000

    get popup label to fade

--- PopupLabel.gd
+++ PopupLabel.gd
@@ -4,8 +4,10 @@ onready var label = get_node("Label")
 onready var tween = get_node("Tween")
 
 func _ready():
-	tween.interpolate_property(label, "visibility/opacity", 1.0, 0.0, 0.3, Tween.TRANS_QUAD, Tween.EASE_OUT)	
+	label.hide()
+	tween.interpolate_property(label, "visibility/opacity", 1.0, 0.0, 0.3, Tween.TRANS_QUAD, Tween.EASE_OUT, 3.0)
 	
 func setText(text):
+	label.show()
 	label.set_text(text)
 	tween.start()
